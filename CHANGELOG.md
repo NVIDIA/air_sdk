842fb28e3ba0fa58fdc9ac57b7aed5b0e620f40b
Mike Robertson <mrobertson@nvidia.com>
2021-08-04 14:11:26 -0400

    Add missing requests dep

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.1.4] - 2021-08-04
+### Fixed
+- `requests` was missing as a required package for `pip` installs
+
 ## [2.1.3] - 2021-07-09
 ### Fixed
 - `_resolve_interface` did not include simulation filter in simulations_interfaces.list() call
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='cumulus-air-sdk',
-    version='2.1.3',
+    version='2.1.4',
     author='Mike Robertson',
     author_email='mrobertson@nvidia.com',
     description='Python SDK for interacting with Cumulus AIR',
@@ -21,5 +21,5 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=['python-dateutil']
+    install_requires=['python-dateutil', 'requests']
 )
